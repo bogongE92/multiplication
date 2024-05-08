b049f6c8ea137b0cbf3c13319daa366e74e6aa02
Lena <112360499+bogongE92@users.noreply.github.com>
2024-05-08 18:48:41 +0900

    first commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	/* server: {
+		port: 4000,
+		host: '0.0.0.0'
+	}, */
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': '/src'
+		}
+	}
+})
