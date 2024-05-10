f7354b689bcf1fa4c90076344ffc9a23661c13e7
Lena <112360499+bogongE92@users.noreply.github.com>
2024-05-10 18:03:19 +0900

    구구단 과제 틀 만들기

--- src/components/Multiplication.vue
+++ src/components/Multiplication.vue
@@ -2,26 +2,58 @@
 	<div class="container">
 		<h1>구구단 과제</h1>
 		<div class="card">
-			<input calss="stage" type="text" placeholder="단수를 입력해 주세요(숫자만)" />
-			<input class="line" type="text" placeholder="출력할 줄을 입력해 주세요(숫자만)" />
-			<button class="execution-btn" type="button" @click="count++">구구단 실행</button>
+			<input v-model.number="inputTime" calss="time" type="text" placeholder="단수를 입력해 주세요(숫자만)" />
+			<input v-model.number="inputLines" class="line" type="text" placeholder="출력할 줄을 입력해 주세요(숫자만)" />
+			<button class="execution-btn" type="button" @click="multiply">구구단 실행</button>
 		</div>
 		<div class="print">
-		
+			<p>{{ inputTime }}</p>
+			<p>{{ inputLines }}</p>
+			<ul>
+				<li v-for="multiply in multiplies" 
+					:key="multiply" 
+					:ref="(el) => itemRefs.push(el.textContent)"
+				>
+					{{ multiply }}
+				</li>
+				<!-- <li v-for="multiply in multiplies" 
+				:key="multiply" 
+				:ref="(el) => multiplies.push(el.textContent)"
+				>
+					{{ multiply }}
+				</li> -->
+			</ul>
 		</div>
 	</div>
 </template>
 
 
 <script>
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
 export default {
 	setup() {
-		const count = ref(0);
+		const inputTime = ref('');
+		// const inputTimes = ref([]);
 
+		const inputLines = ref('');
+		const execution = ref();
+		
+		
+		const multiplies = () => {
+			/* inputTime.value */
+			console.log(inputTime.value)
+			console.log(inputLines.value)
+			inputTime.value * inputLines.value
+
+		}
+
+		
 		return {
-			count
+			inputTime,
+			inputLines,
+			execution,
+			multiplies
 		};
 	},
 };
