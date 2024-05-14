f7ab76e5117b399b25bfc356571690a581f0b74e
Lena <112360499+bogongE92@users.noreply.github.com>
2024-05-14 22:34:34 +0900

    구구단 작업

--- src/components/Multiplication.vue
+++ src/components/Multiplication.vue
@@ -1,21 +1,20 @@
 <template>
 	<div class="container">
 		<h1>구구단 과제</h1>
-		<form class="card" @submit="onSubmitForm">
+		<div class="card">
 			<input v-model="inputTime" type="number" placeholder="단수를 입력해 주세요." />
 			<input v-model="inputLines" type="number" placeholder="출력할 줄을 입력해 주세요." />
-			<button class="execution-btn" type="submit" @click="multiply">구구단 실행</button>
-		</form>
+			<button class="execution-btn" type="button" @click="multipy">구구단 실행</button>
+		</div>
 		<div class="print">
 			<p>{{ inputTime }}</p>
 			<p>{{ inputLines }}</p>
-			<!-- <ul>
-				<li v-for="inputLine in inputLines" 
-					:key="inputLine" 
+			<ul>
+				<li v-for="(number, index) in inputResult" :key="index"
 				> 
-					{{ inputTime }}단 x {{ inputLine }} = {{ inputTime * inputLine }}
+					{{ inputTime }}단 x {{ inputLines }} = {{ number[index] }}
 				</li>
-			</ul> -->
+			</ul>
 		</div>
 	</div>
 </template>
@@ -26,27 +25,30 @@ import { ref } from 'vue';
 
 export default {
 	setup() {
-		const inputTime = ref('');
-		const inputLines = ref('');
+		const inputTime = ref(null);
+		const inputLines = ref(null);
+		const inputResult = ref([]);
 		// const multiply = ref (false);
 
-		const onSubmitForm = (e) => {
-			e.preventDefault();
-			console.log(e);
-			
-		}
-		const multiply = () => {
-			if( inputTime.value === '' || inputLines.value === '') {
+		const multipy = (e) => {
+			// e.preventDefault();
+			// console.log(e);
+
+			// for써서  배열에 넣고 위에서 뿌려준다.
+			// 배열에 push 
+			inputResult.value = inputTime.value
+			if( inputTime.value === null || inputLines.value === null) {
 				alert('숫자를 입력해주세요');
 			}
-		};
+			
+		}
 
 		
 		return {
 			inputTime,
 			inputLines,
-			onSubmitForm,
-			multiply
+			inputResult,
+			multipy
 		};
 	},
 };
